ec3eb7aff5a5a5bc1d02d3da03437324a523a3dd
yujie Wang <yujiewang@yujiedemacbook-pro.local>
2022-07-30 11:51:31 +0800

    second commit

--- main.py
+++ main.py
@@ -6,9 +6,33 @@ from urllib.request import urlopen
 url="http://www.baidu.com"
 resp=urlopen(url)  #打开网址得到的响应
 
-resp.read()
+# print(resp.read().decode("utf-8"))
 
-print(resp.read().decode("utf-8"))
+#字节转换成中文
+trans=resp.read().decode("utf-8")
+
+#将内容写到文件中
+with open("mybaidu.html", mode="w") as f:
+    f.write(trans)
+
+print("over!")
+
+#-------------------------------------------------------------------------------------
+import requests
+query=input("输入一个你感兴趣的名字")
+
+url=f'https://www.sogou.com/web?query={query}'
+headers={
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
+
+resp=requests.get(url, headers=headers)  #处理一个小小的反爬，模拟浏览器访问数据
+
+print(resp)
+
+print(resp.text) #拿到页面源代码
+
+
 
 
 
